b97fb4ae763cf579e9e64fe99cc7cd6bd17ea40e
Ofentse06-dev <matheofentse322@gmail.com>
2026-09-15 11:44:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -70,3 +70,103 @@ function addToCart(name, price) {
     alert(name + " has been added to your cart! 🛒");
 
 }
+/* =========================
+   CAR WASH PRICE
+========================= */
+
+const packageSelect =
+    document.getElementById("package");
+
+const washPrice =
+    document.getElementById("washPrice");
+
+
+if (packageSelect && washPrice) {
+
+    packageSelect.addEventListener(
+        "change",
+        function () {
+
+            let price = 0;
+
+            if (this.value === "Exterior Wash") {
+                price = 50;
+            }
+
+            else if (this.value === "Premium Wash") {
+                price = 80;
+            }
+
+            else if (this.value === "Deluxe Wash") {
+                price = 120;
+            }
+
+            washPrice.textContent =
+                "R" + price;
+
+        }
+    );
+
+}
+
+
+/* =========================
+   CAR WASH BOOKING
+========================= */
+
+const carWashForm =
+    document.getElementById("carWashForm");
+
+
+if (carWashForm) {
+
+    carWashForm.addEventListener(
+        "submit",
+        function (event) {
+
+            event.preventDefault();
+
+            const customer =
+                document.getElementById(
+                    "customerName"
+                ).value;
+
+            const vehicle =
+                document.getElementById(
+                    "vehicle"
+                ).value;
+
+            const selectedPackage =
+                document.getElementById(
+                    "package"
+                ).value;
+
+            const date =
+                document.getElementById(
+                    "date"
+                ).value;
+
+            const time =
+                document.getElementById(
+                    "time"
+                ).value;
+
+
+            alert(
+                "Booking Confirmed! 🚗\n\n" +
+
+                "Customer: " + customer + "\n" +
+
+                "Vehicle: " + vehicle + "\n" +
+
+                "Package: " + selectedPackage + "\n" +
+
+                "Date: " + date + "\n" +
+
+                "Time: " + time
+            );
+
+        }
+    );
+
+}
