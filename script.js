d37560517d7a0c202b0b41557837de5ada3dd979
Ofentse06-dev <matheofentse322@gmail.com>
2026-09-15 11:40:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
+/* =========================
+   RESTAURANT MANAGEMENT SYSTEM
+========================= */
 
+
+/* =========================
+   MENU FILTER
+========================= */
+
+function filterMenu(category) {
+
+    const menuItems =
+        document.querySelectorAll(".menu-card");
+
+    menuItems.forEach(function(item) {
+
+        if (
+            category === "all" ||
+            item.dataset.category === category
+        ) {
+
+            item.style.display = "block";
+
+        } else {
+
+            item.style.display = "none";
+
+        }
+
+    });
+
+}
+
+
+/* =========================
+   CART
+========================= */
+
+function addToCart(name, price) {
+
+    let cart =
+        JSON.parse(localStorage.getItem("cart")) || [];
+
+    const existingItem =
+        cart.find(item => item.name === name);
+
+    if (existingItem) {
+
+        existingItem.quantity++;
+
+    } else {
+
+        cart.push({
+
+            name: name,
+
+            price: price,
+
+            quantity: 1
+
+        });
+
+    }
+
+    localStorage.setItem(
+        "cart",
+        JSON.stringify(cart)
+    );
+
+    alert(name + " has been added to your cart! 🛒");
+
+}
